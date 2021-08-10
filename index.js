a2d1cb92d82b4fb992c326e021f0a3e63d572d8e
Shaurya <shaurya29.98@gmail.com>
2021-08-11 03:27:53 +0530

    commit

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.use("/api",homepageRoutes)
 
+//  /api/v2/  1.get request with new model 
+//  /api/v2/  2.post request with new model 
 
 
 
