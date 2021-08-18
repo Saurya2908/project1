9e9a1a464c2e89c3945fd0bb1c9308ab45da45b9
Shaurya <shaurya29.98@gmail.com>
2021-08-19 03:04:36 +0530

    four

--- views/script/register.js
+++ views/script/register.js
@@ -1,13 +1,14 @@
+alert("connected")
 firstName=document.getElementById('firstName')
 lastName=document.getElementById('lastName')
-userType=document.getElementById('usertype')
+//userType=document.getElementById('usertype')
 password=document.getElementById('exampleInputPassword1')
 
 const  mySubmitFunction = async (e)=>
 {
 e.preventDefault()
-console.log({username:firstName.value , password:password.value, first_name:firstName.value, last_name:lastName.value,user_type:userType.value})
-const res = await axios.post('localhost:5000/api/user/register' , {username:firstName.value , password:password.value, first_name:firstName.value, last_name:lastName.value,user_type:userType.value},{Headers: {
+console.log({username:firstName.value , password:password.value, first_name:firstName.value, last_name:lastName.value,user_type:"normal"})
+const res =await axios.post('http://localhost:5000/api/user/register' , {username:firstName.value , password:password.value, first_name:firstName.value, last_name:lastName.value,user_type:"normal"}, {Headers: {
     'Access-Control-Allow-Origin': 'http://localhost:5501'
 }})
 
