ecb7431bfbf92fc8ad990b8a01c074b8e54c7e97
Shaurya <shaurya29.98@gmail.com>
2021-08-19 03:42:30 +0530

    five

--- views/script/login.js
+++ views/script/login.js
@@ -9,10 +9,20 @@ loginForm = document.getElementById('loginForm')
 const  mySubmitFunction = async (e)=>
 {
 e.preventDefault()
-const res = await axios.post('http://localhost:5000/api/user/login' , {username:emailInput.value , password:passwordInput.value} , {Headers: {
+try {
+    const res = await axios.post('http://localhost:5000/api/user/login' , {username:emailInput.value , password:passwordInput.value} , {Headers: {
     'Access-Control-Allow-Origin': 'http://localhost:5501'
 }})
+const check= await axios.get('http://localhost:5000/api/user/check' ,  {Headers: {
+    'Access-Control-Allow-Origin': 'http://localhost:5501'
+}})
+console.log(res.data,check.data)
+if(res.data.user_type==="normal"){
+    //window.location.href="/views/normalUser.html"
+}
+} catch (error) {
+    alert(error.response.data.msg)
+}
 
-console.log(res.data)
 
 }
--- views/script/register.js
+++ views/script/register.js
@@ -1,17 +1,21 @@
-alert("connected")
+userName=document.getElementById('userName')
 firstName=document.getElementById('firstName')
 lastName=document.getElementById('lastName')
-//userType=document.getElementById('usertype')
+userType=document.getElementById('usertype')
 password=document.getElementById('exampleInputPassword1')
 
 const  mySubmitFunction = async (e)=>
 {
 e.preventDefault()
-console.log({username:firstName.value , password:password.value, first_name:firstName.value, last_name:lastName.value,user_type:"normal"})
-const res =await axios.post('http://localhost:5000/api/user/register' , {username:firstName.value , password:password.value, first_name:firstName.value, last_name:lastName.value,user_type:"normal"}, {Headers: {
+try{
+console.log({username:userName.value , password:password.value, first_name:firstName.value, last_name:lastName.value,user_type:userType.value})
+const res =await axios.post('http://localhost:5000/api/user/register' , {username:userName.value , password:password.value, first_name:firstName.value, last_name:lastName.value,user_type:userType.value}, {Headers: {
     'Access-Control-Allow-Origin': 'http://localhost:5501'
 }})
-
 console.log(res.data)
+}
+catch(error){
+    alert(error.response.data.msg)
+}
 
 }
